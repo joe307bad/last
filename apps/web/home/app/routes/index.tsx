cf247b0dcfba3f0754493f4df7246dd13e8c00c0
joe307bad <joe307bad@gmail.com>
2022-02-27 00:46:08 -0500

    pulling back actual planet data

--- apps/web/home/app/routes/index.tsx
+++ apps/web/home/app/routes/index.tsx
@@ -3,10 +3,7 @@ import type {
   LoaderFunction,
 } from 'remix';
 import { useLoaderData, json } from 'remix';
-import { Planet } from '../components/planet';
-import faker from '@faker-js/faker';
-import elements from '../elements.json';
-import { EventTypes } from '../../../../../libs/shared/types/src';
+import got from 'got';
 
 export type TPlanet = {
   name: string;
@@ -17,95 +14,52 @@ export type TPlanet = {
 };
 
 type IndexData = {
-  planets: Array<TPlanet>;
+  data: {
+    planets: { edges: { node: TPlanet }[] };
+  };
 };
 
 // Loaders provide data to components and are only ever called on the server, so
 // you can connect to a database or run any server side code you want right next
 // to the component that renders it.
 // https://remix.run/api/conventions#loader
-export let loader: LoaderFunction = () => {
-  const elementNames = elements.map(
-    (e: { name: string }) => e.name
-  );
+export let loader: LoaderFunction = async () => {
+  const plantInfoQuery = `
+  query {
+    planets {
+      edges {
+        node {
+          id
+          name
+        }
+      }
+    }
+  }
+ `;
 
-  const capitalizeAllFirstWords = (
-    s: string[]
-  ) => {
-    return s
-      .slice(0, 2)
-      .join(' ')
-      .toLowerCase()
-      .split(' ')
-      .map(
-        (s) =>
-          s.charAt(0).toUpperCase() +
-          s.substring(1)
-      )
-      .join(' ');
-  };
-
-  const twoRandomStrings = () => {
-    const words = [
-      faker.name.firstName(),
-      faker.name.lastName(),
-      faker.lorem.word(),
-      faker.random.word(),
-      faker.random.alpha(),
-    ].sort(() => 0.5 - Math.random());
-
-    return capitalizeAllFirstWords(words);
-  };
-
-  const randomHouse = () =>
-    capitalizeAllFirstWords([
-      faker.name.lastName(),
-      faker.lorem.word(),
-    ]);
-
-  const randomResource = (): [
-    string,
-    number,
-    string
-  ] => {
-    return [
-      faker.helpers.randomize(['+', '-']),
-      faker.datatype.number({
-        max: 3000,
-        min: 100,
-      }),
-      faker.helpers.randomize(elementNames),
-    ];
-  };
-
-  const randomPlanet = (): TPlanet => ({
-    name: twoRandomStrings(),
-    colors: [
-      faker.internet.color(),
-      faker.internet.color(),
-    ],
-    opposingFamilies: [
-      randomHouse(),
-      randomHouse(),
-    ],
-    rulingFamily: randomHouse(),
-    resources: [
-      randomResource(),
-      randomResource(),
-    ],
-  });
-
-  let data: IndexData = {
-    planets: [
-      randomPlanet(),
-      randomPlanet(),
-      randomPlanet(),
-      randomPlanet(),
-    ],
-  };
-
-  // https://remix.run/api/remix#json
-  return json(data);
+  const planets = await got
+    .post('http://localhost:3333/graphql', {
+      json: {
+        query: plantInfoQuery,
+      },
+    })
+    .json<{
+      data: {
+        edges: {
+          node: {
+            name: string;
+            planetResources: {
+              initialAmount: number;
+              resource: {
+                name: string;
+                id: string;
+              };
+            };
+          }[];
+        }[];
+      };
+    }>();
+  return json(planets);
 };
 
 // https://remix.run/api/conventions#meta
@@ -116,50 +70,41 @@ export let meta: MetaFunction = () => {
   };
 };
 
-const exampleEvents = (createDate: any) => [
-  {
-    entityId:
-      '3512846e-7454-4603-be1d-7f2adfbd4e63',
-    entityType: 'planet',
-    eventType: EventTypes.resource_change,
-    valueChange: '-1180',
-    secondaryEntityId:
-      '027882a9-d7c7-4709-aea8-ae316e474c68',
-    createDate,
-  },
-];
-
-const creatExampleEvents = async () => {
-  await fetch('http://localhost:3077/api', {
-    method: 'POST',
-    mode: 'cors',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(
-      exampleEvents(Date.now())
-    ),
-  });
-};
-
 // https://remix.run/guides/routing#index-routes
 export default function Index() {
   let data = useLoaderData<IndexData>();
+  console.log(data.data.planets.edges);
+
+  if (!data?.data?.planets?.edges) {
+    return <></>;
+  }
 
   return (
     <main>
-      <div className="p-2 bg-white text-black">
-        <button
-          onClick={creatExampleEvents}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Create example events
-        </button>
-      </div>
-      <div className="remix__page flex justify-center">
-        {data.planets.map((planet, i) => (
-          <Planet key={i} planet={planet} />
-        ))}
+      <div>
+        {data.data.planets.edges.map(
+          ({ node }, i) => (
+            <div key={i}>
+              {Object.keys(node).map(
+                (planetKey, i) => (
+                  <div key={i}>
+                    <div
+                      style={{ color: 'white' }}
+                    >
+                      {planetKey}:{' '}
+                      {
+                        node[
+                          planetKey as keyof TPlanet
+                        ]
+                      }
+                    </div>
+                  </div>
+                )
+              )}
+              <br />
+            </div>
+          )
+        )}
       </div>
     </main>
   );
---
+++ apps/web/home/app/routes/index_old.tsx
@@ -0,0 +1,166 @@
+import type {
+  MetaFunction,
+  LoaderFunction,
+} from 'remix';
+import { useLoaderData, json } from 'remix';
+import { Planet } from '../components/planet';
+import faker from '@faker-js/faker';
+import elements from '../elements.json';
+import { EventTypes } from '../../../../../libs/shared/types/src';
+
+export type TPlanet = {
+  name: string;
+  colors: string[];
+  rulingFamily: string;
+  opposingFamilies: string[];
+  resources: Array<[string, number, string]>;
+};
+
+type IndexData = {
+  planets: Array<TPlanet>;
+};
+
+// Loaders provide data to components and are only ever called on the server, so
+// you can connect to a database or run any server side code you want right next
+// to the component that renders it.
+// https://remix.run/api/conventions#loader
+export let loader: LoaderFunction = () => {
+  const elementNames = elements.map(
+    (e: { name: string }) => e.name
+  );
+
+  const capitalizeAllFirstWords = (
+    s: string[]
+  ) => {
+    return s
+      .slice(0, 2)
+      .join(' ')
+      .toLowerCase()
+      .split(' ')
+      .map(
+        (s) =>
+          s.charAt(0).toUpperCase() +
+          s.substring(1)
+      )
+      .join(' ');
+  };
+
+  const twoRandomStrings = () => {
+    const words = [
+      faker.name.firstName(),
+      faker.name.lastName(),
+      faker.lorem.word(),
+      faker.random.word(),
+      faker.random.alpha(),
+    ].sort(() => 0.5 - Math.random());
+
+    return capitalizeAllFirstWords(words);
+  };
+
+  const randomHouse = () =>
+    capitalizeAllFirstWords([
+      faker.name.lastName(),
+      faker.lorem.word(),
+    ]);
+
+  const randomResource = (): [
+    string,
+    number,
+    string
+  ] => {
+    return [
+      faker.helpers.randomize(['+', '-']),
+      faker.datatype.number({
+        max: 3000,
+        min: 100,
+      }),
+      faker.helpers.randomize(elementNames),
+    ];
+  };
+
+  const randomPlanet = (): TPlanet => ({
+    name: twoRandomStrings(),
+    colors: [
+      faker.internet.color(),
+      faker.internet.color(),
+    ],
+    opposingFamilies: [
+      randomHouse(),
+      randomHouse(),
+    ],
+    rulingFamily: randomHouse(),
+    resources: [
+      randomResource(),
+      randomResource(),
+    ],
+  });
+
+  let data: IndexData = {
+    planets: [
+      randomPlanet(),
+      randomPlanet(),
+      randomPlanet(),
+      randomPlanet(),
+    ],
+  };
+
+  // https://remix.run/api/remix#json
+  return json(data);
+};
+
+// https://remix.run/api/conventions#meta
+export let meta: MetaFunction = () => {
+  return {
+    title: 'Remix Starter',
+    description: 'Welcome to remix!',
+  };
+};
+
+const exampleEvents = (createDate: any) => [
+  {
+    entityId:
+      '3512846e-7454-4603-be1d-7f2adfbd4e63',
+    entityType: 'planet',
+    eventType: EventTypes.resource_change,
+    valueChange: '-1180',
+    secondaryEntityId:
+      '027882a9-d7c7-4709-aea8-ae316e474c68',
+    createDate,
+  },
+];
+
+const creatExampleEvents = async () => {
+  await fetch('http://localhost:3077/api', {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(
+      exampleEvents(Date.now())
+    ),
+  });
+};
+
+// https://remix.run/guides/routing#index-routes
+export default function Index() {
+  let data = useLoaderData<IndexData>();
+
+  return (
+    <main>
+      <div className="p-2 bg-white text-black">
+        <button
+          onClick={creatExampleEvents}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Create example events
+        </button>
+      </div>
+      <div className="remix__page flex justify-center">
+        {data.planets.map((planet, i) => (
+          <Planet key={i} planet={planet} />
+        ))}
+      </div>
+    </main>
+  );
+}
